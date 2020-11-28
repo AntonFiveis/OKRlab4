5e4348314e52cf139f4ab87b4c470998c859cea0
Anton <antonfiveis@gmail.com>
2020-11-28 23:21:38 +0200

    updating db.json

--- src/js/test.server.js
+++ src/js/test.server.js
@@ -3,9 +3,16 @@ export default class Client {
         this._urlBase = 'http://my-json-server.typicode.com/antonfiveis/OKRlab4/'
     }
 
-    getData(url) {
-        if(url=='')
+    async getData(url) {
+        let urlparts=url.split('/')
+        if(url==='')
             url='db'
+        if(urlparts.length>1&& !(+urlparts[1])){
+            const data = await (await fetch(this._urlBase+urlparts[0])).json()
+            return data.filter((item)=>{
+                return item.features.includes(urlparts[1])
+            })
+        }
         return fetch(this._urlBase + url).then(data => data.json())
     }
 }
